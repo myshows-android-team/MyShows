e2069d752a89902ca35f78c688bd4cc9743e5185
Ivan Samborskiy <whiplash1993@yandex.ru>
2015-06-18 00:52:22 +0300

    Created Login and Main activity for testing authentication.

---
+++ app/src/main/java/me/myshows/android/LoginActivity.java
@@ -0,0 +1,38 @@
+package me.myshows.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import me.myshows.android.api.MyShowsClient;
+
+/**
+ * @author Whiplash
+ * @date 14.06.2015
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (MyShowsClient.get(this).isLogin()) {
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            findViewById(R.id.loginButton).setOnClickListener((view -> {
+                String login = ((EditText) findViewById(R.id.login)).getText().toString();
+                String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                MyShowsClient.get(this).authentication(login, password, isLogin -> {
+                    if (isLogin) {
+                        startActivity(new Intent(this, MainActivity.class));
+                    } else {
+                        Toast.makeText(this, R.string.incorrect_login_or_password, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }));
+        }
+    }
+}
