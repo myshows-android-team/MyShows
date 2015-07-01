41809669acb3b6794784d515da669814d9d34d75
Nilera <whiplash1993@yandex.ru>
2015-07-02 01:40:32 +0400

    Extracted image requset to signle class.

--- app/src/main/java/me/myshows/android/entities/UserShow.java
+++ app/src/main/java/me/myshows/android/entities/UserShow.java
@@ -3,26 +3,13 @@ package me.myshows.android.entities;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
 import rx.Observable;
-import rx.schedulers.Schedulers;
 
 /**
  * @author Whiplash
  * @date 20.06.2015
  */
 public class UserShow {
-
-    public static final String REQUEST_URL = "http://myshows.me/view/%d/";
-    public static final String SELECT_QUERY = "div.presentBlockImg";
-    public static final String ATTR_NAME = "style";
-    public static final String URL_PREFIX = "http:";
-    public static final String URL_SUFFIX = ")";
 
     private final int showId;
     private final String title;
@@ -104,22 +91,6 @@ public class UserShow {
     }
 
     public Observable<String> requestImageUrl() {
-        return Observable.just(showId)
-                .subscribeOn(Schedulers.newThread())
-                .flatMap(id -> {
-                    try {
-                        // TODO: persist url before return
-                        return Observable.just(getImageUrl(id));
-                    } catch (IOException e) {
-                        return Observable.error(e);
-                    }
-                });
-    }
-
-    private static String getImageUrl(int id) throws IOException {
-        Document doc = Jsoup.connect(String.format(REQUEST_URL, id)).get();
-        Elements imageDiv = doc.select(SELECT_QUERY);
-        String attrValue = imageDiv.attr(ATTR_NAME);
-        return attrValue.substring(attrValue.indexOf(URL_PREFIX), attrValue.indexOf(URL_SUFFIX));
+        return ImageRequester.requestImageUrl(showId);
     }
 }
