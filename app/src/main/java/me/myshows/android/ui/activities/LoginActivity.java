f4b14fee4fa9c3e9c296facfc7326cec410725fc
Pendryak Arseniy <arseniy1993@mail.ru>
2015-07-27 00:10:50 +0300

    added text with link to 'new account' text view

--- app/src/main/java/me/myshows/android/ui/activities/LoginActivity.java
+++ app/src/main/java/me/myshows/android/ui/activities/LoginActivity.java
@@ -4,9 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import me.myshows.android.R;
@@ -23,6 +31,8 @@ import rx.android.schedulers.AndroidSchedulers;
  * @date 14.06.2015
  */
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String REGISTER_URL = "http://myshows.me/";
 
     private Subscription subscription;
 
@@ -41,7 +51,10 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
 
-        findViewById(R.id.loginButton).setOnClickListener(view -> {
+        TextView newAccount = (TextView) findViewById(R.id.new_account);
+        setupNewAccountTextView(newAccount);
+
+        findViewById(R.id.login_button).setOnClickListener(view -> {
             String login = ((EditText) findViewById(R.id.login)).getText().toString();
             String password = ((EditText) findViewById(R.id.password)).getText().toString();
             Credentials credentials = Credentials.make(login, password);
@@ -55,6 +68,30 @@ public class LoginActivity extends AppCompatActivity {
             subscription.unsubscribe();
         }
         super.onDestroy();
+    }
+
+    private void setupNewAccountTextView(TextView view) {
+        String register = getString(R.string.register);
+        String newAccount = getString(R.string.new_account, register);
+        int start = newAccount.indexOf(register);
+        int end = start + register.length();
+        SpannableString ss = new SpannableString(newAccount);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(REGISTER_URL));
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                ds.setColor(getResources().getColor(R.color.red_80_opacity));
+                ds.setUnderlineText(true);
+            }
+        };
+        ss.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        view.setText(ss, TextView.BufferType.SPANNABLE);
+        view.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     private boolean hasInternetConnection() {
