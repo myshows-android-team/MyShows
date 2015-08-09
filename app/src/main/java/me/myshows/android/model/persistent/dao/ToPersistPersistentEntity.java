9dd471e28efbd994dbf9012cce6097b28ba3437c
Ivan Samborskiy <whiplash1993@yandex.ru>
2015-08-09 18:51:53 +0300

    Renamed interfaces to convert entity.

--- app/src/main/java/me/myshows/android/model/persistent/dao/ToPersistPersistentEntity.java
+++ app/src/main/java/me/myshows/android/model/persistent/dao/ToPersistPersistentEntity.java
@@ -2,7 +2,7 @@ package me.myshows.android.model.persistent.dao;
 
 import io.realm.RealmObject;
 
-public interface ToPersistConverter<T> {
+public interface ToPersistPersistentEntity<T> {
 
     RealmObject toRealmObject(T entity);
 }
