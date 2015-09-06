81de881e82a1e31f725555f9e1fa50a6223c8c51
Ivan Samborskiy <whiplash1993@yandex.ru>
2015-09-06 13:09:25 +0300

    Added some new preferences (cache size, notification).

--- app/src/main/java/me/myshows/android/ui/fragments/SettingsFragment.java
+++ app/src/main/java/me/myshows/android/ui/fragments/SettingsFragment.java
@@ -5,11 +5,12 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
-import android.util.Log;
 
 import com.bumptech.glide.Glide;
 
@@ -24,48 +25,44 @@ import me.myshows.android.R;
  */
 public class SettingsFragment extends PreferenceFragment {
 
-    public static final String RINGTONE = "ringtone";
+    public static final String COMPACT_MODE = "compact_mode";
     public static final String CLEAR_CACHE = "clear_cache";
+    public static final String CACHE_SIZE = "cache_size";
+    public static final String CHECK_NEW_SERIES = "check_new_series";
+    public static final String RINGTONE = "ringtone";
+    public static final String VIBRATION = "vibration";
 
     private static final String TAG = SettingsFragment.class.getSimpleName();
+
+    private CheckBoxPreference compactModePreference;
+    private Preference clearCachePreference;
+    private ListPreference cacheSizePreference;
+    private CheckBoxPreference checkNewSeriesPreference;
+    private RingtonePreference ringtonePreference;
+    private CheckBoxPreference vibrationPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.myshows_preference);
+
+        compactModePreference = (CheckBoxPreference) findPreference(COMPACT_MODE);
+        clearCachePreference = findPreference(CLEAR_CACHE);
+        cacheSizePreference = (ListPreference) findPreference(CACHE_SIZE);
+        checkNewSeriesPreference = (CheckBoxPreference) findPreference(CHECK_NEW_SERIES);
+        ringtonePreference = (RingtonePreference) findPreference(RINGTONE);
+        vibrationPreference = (CheckBoxPreference) findPreference(VIBRATION);
+
+        clearCachePreferenceInitialize();
+        checkNewSeriesPreferenceInitialize();
         ringtonePreferenceInitialize();
-        cachePreferenceInitialize();
     }
 
-    private void ringtonePreferenceInitialize() {
-        RingtonePreference preference = (RingtonePreference) findPreference(RINGTONE);
-        setRingtoneName(preference);
-        preference.setOnPreferenceChangeListener((pref, value) -> {
-            setRingtoneName(preference, value.toString());
-            return true;
-        });
-    }
-
-    private void setRingtoneName(RingtonePreference preference) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        setRingtoneName(preference, preferences.getString(RINGTONE, null));
-    }
-
-    private void setRingtoneName(RingtonePreference preference, String uri) {
-        if (uri == null || uri.isEmpty()) {
-            preference.setSummary(getString(R.string.none_ringtone));
-        } else {
-            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), Uri.parse(uri));
-            preference.setSummary(ringtone.getTitle(getActivity()));
-        }
-    }
-
-    private void cachePreferenceInitialize() {
-        Preference cachePreference = findPreference(CLEAR_CACHE);
+    private void clearCachePreferenceInitialize() {
         long bytes = getCacheSize(Glide.getPhotoCacheDir(getActivity()));
-        cachePreference.setSummary(FileUtils.byteCountToDisplaySize(bytes));
-        cachePreference.setOnPreferenceClickListener(preference -> {
-            Log.d(TAG, "OLOLO");
+        clearCachePreference.setSummary(FileUtils.byteCountToDisplaySize(bytes));
+        clearCachePreference.setOnPreferenceClickListener(preference -> {
+            new Thread(() -> Glide.get(getActivity()).clearDiskCache()).start();
             return true;
         });
     }
@@ -83,4 +80,33 @@ public class SettingsFragment extends PreferenceFragment {
         return bytes;
     }
 
+    private void checkNewSeriesPreferenceInitialize() {
+        checkNewSeriesPreference.setOnPreferenceChangeListener((preference, value) -> {
+            ringtonePreference.setEnabled((Boolean) value);
+            vibrationPreference.setEnabled((Boolean) value);
+            return true;
+        });
+    }
+
+    private void ringtonePreferenceInitialize() {
+        setRingtoneName(ringtonePreference);
+        ringtonePreference.setOnPreferenceChangeListener((preference, value) -> {
+            setRingtoneName(ringtonePreference, value.toString());
+            return true;
+        });
+    }
+
+    private void setRingtoneName(RingtonePreference preference) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        setRingtoneName(preference, preferences.getString(RINGTONE, null));
+    }
+
+    private void setRingtoneName(RingtonePreference preference, String uri) {
+        if (uri == null || uri.isEmpty()) {
+            preference.setSummary(getString(R.string.none_ringtone));
+        } else {
+            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), Uri.parse(uri));
+            preference.setSummary(ringtone.getTitle(getActivity()));
+        }
+    }
 }
