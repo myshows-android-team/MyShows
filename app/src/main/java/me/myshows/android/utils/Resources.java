a42191eceb544a0322ee2961c0935e1f22db8906
Pendryak Arseniy <arseniy1993@mail.ru>
2015-10-23 23:55:33 +0300

    resource util class to avoid using depracated methods

---
+++ app/src/main/java/me/myshows/android/utils/Resources.java
@@ -0,0 +1,44 @@
+package me.myshows.android.utils;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by warrior on 24.10.15.
+ */
+public class Resources {
+
+    private Resources() {
+    }
+
+    @Nullable
+    public static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return context.getResources().getDrawable(id);
+        }
+        return context.getDrawable(id);
+    }
+
+    @ColorInt
+    public static int getColor(@NonNull Context context, @ColorRes int id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return context.getResources().getColor(id);
+        }
+        return context.getColor(id);
+    }
+
+    @Nullable
+    public static ColorStateList getColorStateList(@NonNull Context context, @ColorRes int id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return context.getResources().getColorStateList(id);
+        }
+        return context.getColorStateList(id);
+    }
+}
